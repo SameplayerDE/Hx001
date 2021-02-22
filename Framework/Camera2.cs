a30d9c23f3de59c766bafc958676f8a196888872
SameplayerDE <asameplayer@gmail.com>
2021-02-22 11:15:46 +0100

    3

---
+++ Framework/Camera2.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace Hx001.Framework
+{
+    public class Camera2
+    {
+        public Transform2 Transform;
+
+        public Matrix View;
+        public Matrix Projection;
+    }
+}
---
+++ Framework/Camera3.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace Hx001.Framework
+{
+    public class Camera3
+    {
+        public Transform3 Transform;
+
+        public Matrix View;
+        public Matrix Projection;
+    }
+}
