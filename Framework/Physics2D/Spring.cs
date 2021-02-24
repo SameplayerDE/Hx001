c2dc73889c235b8a972ef3f59248ff1060612935
SameplayerDE <asameplayer@gmail.com>
2021-02-24 20:29:42 +0100

    7

---
+++ Framework/Physics2D/Spring.cs
@@ -0,0 +1,20 @@
+﻿namespace Hx001.Framework.Physics2D
+{
+    public class Spring
+    {
+
+        public Particle A;
+        public Particle B;
+        public float Stiffness;
+        public float Lenght;
+
+        public Spring(Particle a, Particle b, float stiffness, float lenght)
+        {
+            A = a;
+            B = b;
+            Stiffness = stiffness;
+            Lenght = lenght;
+        }
+
+    }
+}
